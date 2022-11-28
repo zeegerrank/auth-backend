c48a29067edd5726600607b14f599931b6404002
zeegerrank <zeegerrank@gmail.com>
2022-11-28 13:50:09 +0700

    commit

---
+++ db/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
+});
+
+const User = mongoose.model("users", UserSchema);
+module.exports = User;
