0cb2e250fef5a435665a01ef204a3ac63460a8b2
zeegerrank <zeegerrank@gmail.com>
2022-11-28 16:59:40 +0700

    commit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const http = require("http");
 const app = require("./app");
 
-// const normalizePort = val => {
-//   const port = parseInt(val, 10);
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-//   if (isNaN(port)) {
-//     return val;
-//   }
-//   if (port >= 0) {
-//     return port;
-//   }
-//   return false;
-// };
-const port = process.env.PORT || "3000";
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 const errorHandler = (error) => {
