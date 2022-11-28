ba425ac24dc58241cfc9562e9283ad4de60e1dd7
zeegerrank <zeegerrank@gmail.com>
2022-11-28 16:01:59 +0700

    commit

--- db/userModel.js
+++ db/userModel.js
@@ -12,5 +12,5 @@ const mongoose = require("mongoose");const UserSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model.UserSchema;
+const User = mongoose.model("user", UserSchema);
 module.exports = User;
