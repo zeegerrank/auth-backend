3b0c6327f3322aca1d11abb6d765381b7db5d7b4
zeegerrank <zeegerrank@gmail.com>
2022-11-28 15:35:59 +0700

    commit

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+
+const User = require("./db/userModel");
 
 // body parser configuration
 app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// require database connection
+const dbConnect = require("./db/dbConnect");
+
+// execute database connection
+dbConnect();
 
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
 });
 
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
 
 module.exports = app;
